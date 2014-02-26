7953ff847bb03a09064937d828ac66898d23ed91
Kyle Rudzinski <kyle_rudzinski@mba.berkeley.edu>
2014-02-26 03:30:33 +0000

    Made new static pages controller

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "should have the content 'Demo App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Demo App')
+   end
+  end
+end
